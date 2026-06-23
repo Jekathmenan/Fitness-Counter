788541fe550417b101773346de373208b30f6420
Jekath <>
2026-06-23 17:12:19 +0200

    Configure Main Method

---
+++ src/main/java/ch/fhnw/fitnesscounter/FitnesscounterApplication.java
@@ -0,0 +1,21 @@
+package ch.fhnw.fitnesscounter;
+
+import ch.fhnw.fitnesscounter.config.RsaKeyProperties;
+import ch.fhnw.fitnesscounter.controller.BodypartController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
+
+@EnableConfigurationProperties(RsaKeyProperties.class)
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
+public class FitnesscounterApplication {
+	private static final Logger log = LoggerFactory.getLogger(BodypartController.class);
+	public static void main(String[] args) {
+		SpringApplication.run(FitnesscounterApplication.class, args);
+	}
+
+}
