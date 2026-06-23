7eca808fb18899468e2c30566918f156bfbb638d
Jekath <>
2026-06-23 18:43:47 +0200

    Add Controllers - Adding controller-skeletons for bodypart, exercise and workout

---
+++ src/main/java/ch/fhnw/fitnesscounter/controller/BodypartController.java
@@ -0,0 +1,12 @@
+package ch.fhnw.fitnesscounter.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/body-part")
+@RequiredArgsConstructor
+public class BodypartController {
+
+}
---
+++ src/main/java/ch/fhnw/fitnesscounter/controller/WorkoutController.java
@@ -0,0 +1,11 @@
+package ch.fhnw.fitnesscounter.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/workout")
+@RequiredArgsConstructor
+public class WorkoutController {
+}
