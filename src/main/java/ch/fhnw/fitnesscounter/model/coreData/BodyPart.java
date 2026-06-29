7d89ee998fd5138fb28fd5bf8dbcff2ca5770406
Jekath <>
2026-06-29 22:17:33 +0200

    Add Helper Method to BodyPart Entity-Model

--- src/main/java/ch/fhnw/fitnesscounter/model/coreData/BodyPart.java
+++ src/main/java/ch/fhnw/fitnesscounter/model/coreData/BodyPart.java
@@ -1,6 +1,8 @@
-package ch.fhnw.fitnesscounter.model.bodypart;
+package ch.fhnw.fitnesscounter.model.coreData;
 
+import ch.fhnw.fitnesscounter.dto.core.BodyPartDto;
 import ch.fhnw.fitnesscounter.model.BaseEntity;
+import ch.fhnw.fitnesscounter.util.DataNormalizer;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -21,6 +23,17 @@ public class BodyPart extends BaseEntity  {
 
     @Column(nullable = true)
     private String description;
+
+    /**
+     *
+     * Diese Helfermethode übernimmt die Werte einer DTO in die eigenen Attribute und unterstützt somit die Update-Route.
+     *
+     * @param dto
+     */
+    public void updateFrom(BodyPartDto dto) {
+        this.name = DataNormalizer.normalizeString(dto.name());
+        this.description = dto.description();
+    }
 
     // TODO: User should be able to request a Body Part. A requested bodypart is available for that user once a admin grants that request that bodypart is available for all users
 }
