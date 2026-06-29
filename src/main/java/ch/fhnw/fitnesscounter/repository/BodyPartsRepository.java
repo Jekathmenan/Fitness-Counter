e013aafad5ae95b8942b8881ec808ed2b47b6f1a
Jekath <>
2026-06-29 22:18:33 +0200

    Add BodyPartsRepository

--- src/main/java/ch/fhnw/fitnesscounter/repository/BodyPartsRepository.java
+++ src/main/java/ch/fhnw/fitnesscounter/repository/BodyPartsRepository.java
@@ -1,6 +1,6 @@
 package ch.fhnw.fitnesscounter.repository;
 
-import ch.fhnw.fitnesscounter.model.bodypart.BodyPart;
+import ch.fhnw.fitnesscounter.model.coreData.BodyPart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
