b9526de04c8670d6abeaf869d54b2af33c373000
Jekath <>
2026-06-29 22:16:20 +0200

    Add Routes to BodyPartsController  - Add Route "add-many"  - Add Method PUT  - Add Method DELETE

--- src/main/java/ch/fhnw/fitnesscounter/controller/BodypartController.java
+++ src/main/java/ch/fhnw/fitnesscounter/controller/BodypartController.java
@@ -1,7 +1,8 @@
 package ch.fhnw.fitnesscounter.controller;
 
 import ch.fhnw.fitnesscounter.dto.core.BodyPartDto;
-import ch.fhnw.fitnesscounter.model.bodypart.BodyPart;
+import ch.fhnw.fitnesscounter.model.coreData.BodyPart;
+import ch.fhnw.fitnesscounter.dto.core.BodyPartsDTO;
 import ch.fhnw.fitnesscounter.repository.BodyPartsRepository;
 import ch.fhnw.fitnesscounter.service.coreData.BodyPartsService;
 import jakarta.validation.Valid;
@@ -40,5 +41,23 @@ public class BodypartController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createBodyPart(@Valid @RequestBody BodyPartDto bodyPart, Principal principal) {
         bodyPartsService.createBodyPart(bodyPart, principal.getName());
+    }
+
+    @PostMapping("/add-many")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createMultipleBodyParts(@Valid BodyPartsDTO bodyParts, Principal principal) {
+        bodyPartsService.createMany(bodyParts.bodyParts(), principal.getName());
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void updateBodyPart(@PathVariable Long id, @Valid @RequestBody BodyPartDto bodyPartDto, Principal principal) {
+        bodyPartsService.updateBodyPart(id, bodyPartDto, principal.getName());
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void deleteBodyPart(@PathVariable Long id, Principal principal) {
+        bodyPartsService.deleteBodyPart(id, principal.getName());
     }
 }
