c2502db87462ca0cab0ac55459eb68c342a29062
Jekath <>
2026-06-29 22:19:33 +0200

    Add Service Methods to BodyPartsService  - Add Method createMany  - Add Method updateBodyPart  - Add Method deleteBodyPart

--- src/main/java/ch/fhnw/fitnesscounter/service/coreData/BodyPartsService.java
+++ src/main/java/ch/fhnw/fitnesscounter/service/coreData/BodyPartsService.java
@@ -1,13 +1,17 @@
 package ch.fhnw.fitnesscounter.service.coreData;
 
 import ch.fhnw.fitnesscounter.dto.core.BodyPartDto;
-import ch.fhnw.fitnesscounter.model.bodypart.BodyPart;
+import ch.fhnw.fitnesscounter.exception.FitnessAPIException;
+import ch.fhnw.fitnesscounter.model.coreData.BodyPart;
 import ch.fhnw.fitnesscounter.repository.BodyPartsRepository;
 import ch.fhnw.fitnesscounter.util.DataNormalizer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -33,5 +37,56 @@ public class BodyPartsService {
 
         bodyPartsRepository.save(bodyPart);
         log.info("Neuer Körperteil von {} angelegt {}", user, bodyPartDto.name());
+    }
+
+    /**
+     *
+     * Diese Methode erlaubt die Erfassung von mehreren Körperteilen.
+     *
+     * @param dtos
+     * @param user
+     */
+    public void createMany (List<BodyPartDto> dtos, String user) {
+        for(BodyPartDto dto : dtos) {
+            createBodyPart(dto, user);
+        }
+        // dtos.forEach(dto -> createBodyPart(dto, user));
+    }
+
+    /**
+     *
+     * Diese Methode ist für das Ändern eines bestehenden Körperteils zuständig.
+     *
+     * @param id
+     * @param dto
+     * @param user
+     */
+    public void updateBodyPart(long id, BodyPartDto dto, String user) {
+        bodyPartsRepository.findById(id).ifPresentOrElse(bodyPart1 -> {
+            bodyPart1.updateFrom(dto);
+            bodyPartsRepository.save(bodyPart1);
+        },
+        () -> {
+            log.warn("User {} tried to update a non existing body part {}.", user, id);
+            throw new FitnessAPIException("Body part not found.", HttpStatus.NOT_FOUND);
+        });
+    }
+
+    /**
+     *
+     * Diese Methode ist für das Löschen eines Körperteils zuständig.
+     *
+     * @param id
+     * @param user
+     */
+    public void deleteBodyPart (long id, String user) {
+
+        bodyPartsRepository.findById(id).ifPresentOrElse(
+                bodyPartsRepository::delete,
+                () -> {
+                    log.warn("User {} tried to delete a non existing body part {}.", user, id);
+                    throw new FitnessAPIException("Body part not found.", HttpStatus.NOT_FOUND);
+                }
+        );
     }
 }
