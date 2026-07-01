82fa123eb153e0ef29f42933e99f697eb077fb4b
Jekath <>
2026-07-01 21:14:27 +0200

    Modify ExerciseController  - Move Controller into subpackage coreData  - Add Route "/" to create new Exercise

---
+++ src/main/java/ch/fhnw/fitnesscounter/controller/coreData/ExerciseController.java
@@ -0,0 +1,24 @@
+package ch.fhnw.fitnesscounter.controller.coreData;
+
+import ch.fhnw.fitnesscounter.dto.coreData.ExerciseDto;
+import ch.fhnw.fitnesscounter.model.coreData.Exercise;
+import ch.fhnw.fitnesscounter.service.coreData.ExerciseService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/exercise")
+@RequiredArgsConstructor
+public class ExerciseController {
+    private final ExerciseService exerciseService;
+
+    @PostMapping("/")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createExercise (@Valid @RequestBody ExerciseDto exerciseDto, Principal principal) {
+        exerciseService.createExercise(exerciseDto, principal.getName());
+    }
+}
