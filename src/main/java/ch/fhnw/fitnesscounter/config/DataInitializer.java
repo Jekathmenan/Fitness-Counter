c25dfa1193ebb4c641d7a58123c54a532c18ad29
Jekath <>
2026-07-01 21:01:27 +0200

    Update DataInitializer  - Seed Database with default body parts

--- src/main/java/ch/fhnw/fitnesscounter/config/DataInitializer.java
+++ src/main/java/ch/fhnw/fitnesscounter/config/DataInitializer.java
@@ -1,12 +1,19 @@
 package ch.fhnw.fitnesscounter.config;
 
+import ch.fhnw.fitnesscounter.dto.coreData.BodyPartsListDto;
 import ch.fhnw.fitnesscounter.model.auth.Role;
 import ch.fhnw.fitnesscounter.model.auth.User;
+import ch.fhnw.fitnesscounter.repository.BodyPartsRepository;
 import ch.fhnw.fitnesscounter.repository.UserRepository;
+import ch.fhnw.fitnesscounter.service.coreData.BodyPartsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.asm.TypeReference;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.InputStream;
 
 /**
  *
@@ -19,6 +26,9 @@ public class DataInitializer implements CommandLineRunner {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final BodyPartsRepository bodyPartsRepository;
+    private final BodyPartsService bodyPartsService;
+    private final ObjectMapper objectMapper;
 
     /**
      *
@@ -29,6 +39,10 @@ public class DataInitializer implements CommandLineRunner {
     @Override
     public void run(String... args) {
         // Erstelle den Admin-Benutzer, wenn dieser nicht schon existiert
+        seedUsers();
+        seedDefaultBodyParts();
+    }
+
         if (userRepository.findByEmail("admin@fitness.ch").isEmpty()) {
             User admin = User.builder()
                     .email("admin@fitness.ch")
@@ -40,6 +54,24 @@ public class DataInitializer implements CommandLineRunner {
                     .build();
 
             userRepository.save(admin);
+        }
+    }
+
+    /**
+     * Diese Methode lädt die Körperteile aus der JSON-Datei /data/bodyparts.json in die Datenbank.
+     */
+    private void seedDefaultBodyParts() {
+        if (bodyPartsRepository.count() == 0) {
+            // Lade JSON File
+            try (InputStream inputStream = TypeReference.class.getResourceAsStream("/data/bodyparts.json")) {
+                // Mappe JSON-Stream als BodyPartsListDto
+                BodyPartsListDto bodyPartsListDto = objectMapper.readValue(inputStream, BodyPartsListDto.class);
+
+                // Übergebe bodyPartList an den Service
+                bodyPartsService.createMany(bodyPartsListDto.getBodyParts(), "admin@fitness.ch");
+            } catch (Exception e) {
+                throw new RuntimeException("Fehler beim Füllen der Datenbank mit Initialdaten.", e);
+            }
         }
     }
 }
