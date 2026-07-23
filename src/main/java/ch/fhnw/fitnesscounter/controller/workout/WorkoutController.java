87c1958bbe322a3a820f553d0bc31b6f0f2532b3
Jekath <>
2026-07-23 07:06:09 +0200

    Add Routes to WorkoutController  - Add /active Route  - Add /start Route  - Add /end Route

--- src/main/java/ch/fhnw/fitnesscounter/controller/workout/WorkoutController.java
+++ src/main/java/ch/fhnw/fitnesscounter/controller/workout/WorkoutController.java
@@ -6,8 +6,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.security.Principal;
 import java.util.List;
 
@@ -24,16 +24,33 @@ public class WorkoutController {
         return workoutService.getAllWorkoutsByUser(principal.getName());
     }
 
-    @GetMapping("/get-all")
+    @GetMapping("/all")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public List<WorkoutDto> getAllWorkouts () {
         return workoutService.getAllWorkouts();
     }
 
-    @PostMapping("/")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void createWorkout (@Valid @RequestBody WorkoutDto workoutDto) {
+    @GetMapping("/active")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ResponseEntity<WorkoutDto> getActiveWorkout (Principal principal) {
+        WorkoutDto dto = workoutService.getActiveWorkoutForUser(principal.getName());
 
+        if (dto == null)
+            return ResponseEntity.noContent().build();
+
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping("/start")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<WorkoutDto> startWorkout (@Valid @RequestBody WorkoutDto workoutDto, Principal principal) {
+        return ResponseEntity.ok(workoutService.startWorkout(workoutDto, principal.getName()));
+    }
+
+    @PostMapping("/end/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void endWorkout (@PathVariable Long id, Principal principal) {
+        workoutService.endWorkout(id, principal.getName());
     }
 
 }
