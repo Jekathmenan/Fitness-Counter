bc1b8aec0a394c02160746f3aac68cd11acfffa7
Jekath <>
2026-07-23 07:04:16 +0200

    Implement Helper-Methods to WorkoutRepository

--- src/main/java/ch/fhnw/fitnesscounter/repository/WorkoutRepository.java
+++ src/main/java/ch/fhnw/fitnesscounter/repository/WorkoutRepository.java
@@ -3,11 +3,12 @@ package ch.fhnw.fitnesscounter.repository;
 import ch.fhnw.fitnesscounter.model.coreData.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface WorkoutRepository extends JpaRepository<Workout, Long>  {
     List<Workout> findByUserId(Long userId);
-    List<Workout> findByUserIdAndEndTimeIsNotNull(Long userId);
+    Optional<Workout> findByIdAndEndTimeIsNull(Long userId);
+    Optional<Workout> findFirstByUserIdAndEndTimeIsNull(Long userId);
 }
