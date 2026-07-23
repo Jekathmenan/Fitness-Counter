7eee7017aaf399d0522cde3edabfddda2e86b351
Jekath <>
2026-07-23 07:03:34 +0200

    Implement WorkoutExerciseRepository

---
+++ src/main/java/ch/fhnw/fitnesscounter/repository/WorkoutExerciseRepository.java
@@ -0,0 +1,9 @@
+package ch.fhnw.fitnesscounter.repository;
+
+import ch.fhnw.fitnesscounter.model.coreData.WorkoutExercise;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkoutExerciseRepository extends JpaRepository<WorkoutExercise, Long> {
+}
