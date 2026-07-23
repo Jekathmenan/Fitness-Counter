9ec8aba7c986c385591b352a7cdc90ee829c51b2
Jekath <>
2026-07-23 07:03:11 +0200

    Add findByName to WorkoutRepository

--- src/main/java/ch/fhnw/fitnesscounter/repository/ExerciseRepository.java
+++ src/main/java/ch/fhnw/fitnesscounter/repository/ExerciseRepository.java
@@ -3,8 +3,10 @@ package ch.fhnw.fitnesscounter.repository;
 import ch.fhnw.fitnesscounter.model.coreData.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
     boolean existsByName(String name);
+    Optional<Exercise> findByName(String name);
 }
