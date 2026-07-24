b3f43311672e389e3a651e0655f7a4c1343f62f8
Jekath <>
2026-07-24 21:03:41 +0200

    Add HelperMethod to WorkoutSetDto  - toEntity()

--- src/main/java/ch/fhnw/fitnesscounter/dto/workout/WorkoutSetDto.java
+++ src/main/java/ch/fhnw/fitnesscounter/dto/workout/WorkoutSetDto.java
@@ -1,9 +1,22 @@
 package ch.fhnw.fitnesscounter.dto.workout;
 
+import ch.fhnw.fitnesscounter.model.coreData.WorkoutSet;
+import jakarta.annotation.Nullable;
+
 public record WorkoutSetDto(
+        @Nullable
         Long id,
         Double weight,
         Integer reps,
+        @Nullable
         Integer setOrder
 ) {
+    public WorkoutSet toEntity() {
+        WorkoutSet set = new WorkoutSet();
+        set.setId(id);
+        set.setWeight(weight);
+        set.setReps(reps);
+        set.setSetOrder(setOrder);
+        return set;
+    }
 }
